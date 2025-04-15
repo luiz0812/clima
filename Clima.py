c03624c0a6482c8cd136df8f0bcf10fc7d54f0a8
luiz0812 <Luizfelipedasilvagosmes@gmail.com>
2025-04-14 21:28:58 -0300

    Add files via upload

---
+++ Clima.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.page_link("Clima.py", label="Clima", icon="🏠")
+st.page_link("pages/Belo_Horizonte.py", label="Belo Horizonte")
+st.page_link("pages/Conselheiro_Lafaiete.py", label="Conselheiro Lafaiete")
+st.page_link("pages/Ouro_Branco.py", label="Ouro Branco")
---
+++ pages/Belo_Horizonte.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+from bs4 import BeautifulSoup
+import requests
+#cidade = input('Digite a cidade').replace(' ', '-').lower()
+#print(cidade)
+site ='https://www.accuweather.com/pt/br/belo-horizonte/44403/weather-forecast/44403'
+#print(site)
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
+}
+
+
+resposta = requests.get(site ,headers=headers,verify=False)
+sopa = BeautifulSoup(resposta.text , 'html.parser')
+
+climas= sopa.find_all('span' , class_="temp-hi")
+localidade = sopa.find('h1')
+previsao = sopa.find_all('p',class_="no-wrap")
+dias = sopa.find_all('p',class_="day")
+
+for i, clima in enumerate(climas):
+    st.write(f'### {dias[i].text.capitalize()}\n ', f'\n #### A temperatura é: {clima.text}C\n' , f'\nE a previsao para o dia será de :  {previsao[i].text}')
+    st.write('-'*50)
